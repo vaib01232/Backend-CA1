b0c025a925f634c92182890698e9045313ce4fcc
Vaibhaav BS <119183394+vaib01232@users.noreply.github.com>
2025-02-24 15:15:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-const express=require('express');
-const app=express();
-const userRouter=require('./Controllers/User');
 
-const port=3000;
-app.listen(port,()=>{
-    console.log(`Server is running on the port ${port}`)
-})
-
-app.use(express.json());
-
-app.use('/login',userRouter);
